b9d044994e510cab4b1b6b40cb3a603f4011d987
Sarimsaljook <80625774+Sarimsaljook@users.noreply.github.com>
2023-07-13 13:47:29 -0400

    Change naivgation container lib

--- Bob/App.js
+++ Bob/App.js
@@ -4,20 +4,20 @@ import WelcomeScreen from './WelcomeScreen';
 import Login from './Login';
 import SignUp from './SignUp';
 
-import { BaseNavigationContainer } from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <BaseNavigationContainer>
+    <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Welcome" component={WelcomeScreen}/>
         <Stack.Screen name="Login" component={Login}/>
         <Stack.Screen name="SignUp" component={SignUp}/>
       </Stack.Navigator>
-    </BaseNavigationContainer>
+    </NavigationContainer>
   );
 }
 
