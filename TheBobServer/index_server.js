133ee735d47a864e6edcae43fb79a2b64c1314f7
Sarimsaljook <80625774+Sarimsaljook@users.noreply.github.com>
2023-08-07 17:06:02 +0500

    Add Stock Rank logic to main server script

--- TheBobServer/index_server.js
+++ TheBobServer/index_server.js
@@ -17,7 +17,7 @@ const tableName = 'Users';
 // Declare Dynao DB instance
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
-const API_KEY = 'Q7FDGNSK8YH7JRYT';
+const API_KEY = 'ZCQDT96VBC5WB0KO';
 const BASE_URL = 'https://www.alphavantage.co/query';
 
 // Add end point to Create a new record in DynamoDB
@@ -121,8 +121,107 @@ app.post('/api/getInvestmentPrefferences', (req, res) => {
   }
 });
 
-app.get('/api/getBestToInvest', (req, res) => { 
+// STOCK RANKER LOGIC
+
+// Step 1: Fetch all stock tickers using Alpha Vantage API
+async function getAllStockTickers() {
+    try {
+      const response = await axios.get(BASE_URL, {
+        params: {
+          function: 'LISTING_STATUS',
+          apikey: API_KEY,
+        },
+      });
   
+      const { data } = response;
+      const lines = data.split('\n');
+  
+      // Initialize an array to store tickers
+      const tickers = [];
+  
+      // Start from index 1 to skip the column names in the first row
+      for (let i = 1; i < lines.length; i++) {
+        const line = lines[i];
+
+        const ticker = line.split(',')[0]; // Assuming ticker is the first part of each row
+        if (ticker) {
+          tickers.push(ticker);
+        }
+      }
+  
+      return tickers;
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  // Step 2: Fetch stock data for a given symbol using Alpha Vantage API
+  async function getStockData(symbol) {
+    try {
+      // Check if the symbol is a string before making the API call
+      if (typeof symbol !== 'string') {
+        throw new Error(`Invalid stock symbol: ${symbol}`);
+      }
+  
+      const response = await axios.get(BASE_URL, {
+        params: {
+          function: 'GLOBAL_QUOTE',
+          symbol: symbol,
+          apikey: API_KEY,
+        },
+      });
+  
+      const { data } = response;
+      if (data['Global Quote']) {
+          const price = parseFloat(data['Global Quote']['05. price']);
+          const previousClose = parseFloat(data['Global Quote']['08. previous close']);
+          const percentChange = parseFloat(data['Global Quote']['10. change percent']);
+        return {
+          symbol: data['Global Quote']['01. symbol'],
+          price: price,
+          previousClose: previousClose,
+          percentChange: percentChange,
+        };
+      }
+    } catch (error) {
+      return null; // Return null if there's an error fetching data for the symbol
+    }
+  }
+
+app.get('/api/getBestToInvest', async (req, res) => { 
+  try {
+    // Fetch all available stock tickers
+    const symbols = await getAllStockTickers();
+
+    console.log(symbols);
+
+    // Fetch stock data for all tickers concurrently
+    const stockDataPromises = symbols.map((symbol) => getStockData(symbol));
+    const stockDataResults = await Promise.all(stockDataPromises);
+  
+    // Filter out null values (skipped stocks) from the results
+    const stockData = stockDataResults.filter((data) => data !== null);
+
+    // Sort the stocks in descending order based on percent change
+    stockData.sort((a, b) => b.percentChange - a.percentChange);
+
+    const finalTop5List = [];
+
+    // Display the top 5 ranked stocks based on percent change
+    console.log('Top 5 stocks based on percent change:');
+    for (let i = 0; i < 5; i++) {
+      finalTop5List.push(stockData[i].symbol);
+      console.log(
+        `${i + 1}. ${stockData[i].symbol} - ${stockData[i].percentChange.toFixed(2)}%`
+      );
+    }
+    
+    res.send(finalTop5List);
+
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
 });
 
 // Start the server
